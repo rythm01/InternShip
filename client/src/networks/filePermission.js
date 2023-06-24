502b1e55e33127349ec3e359a46676e17794cd34
Himanshu yadav <himanshuyadavh05@gmail.com>
2023-06-24 16:05:42 +0530

    feat:delete-permission

--- client/src/networks/filePermission.js
+++ client/src/networks/filePermission.js
@@ -1,36 +1,40 @@
-import axios from '.'
-
-
-
-
+import axios from ".";
 
 export const addBuddiesToFilePermissionApi = async (t, data) => {
-    try {
+  try {
+    return axios.post(`/permissions`, data, {
+      headers: {
+        Authorization: `Bearer ${t}`,
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (err) {
+    return err;
+  }
+};
 
-        return axios.post(`/permissions`, data, {
-            headers: {
-                Authorization: `Bearer ${t}`,
-                'Content-Type': 'application/json'
-
-            }
-        })
-
-    } catch (err) {
-        return err
-    }
-}
-
+export const deleteBuddiesToFilePermissionApi = async (t, id, buddyId) => {
+  try {
+    return axios.delete(`/permissions/${id}/${buddyId}`, {
+      headers: {
+        Authorization: `Bearer ${t}`,
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (err) {
+    return err;
+  }
+};
 
 export const getFileAccessApi = async (t, id) => {
-    try {
-        return axios.get(`/permissions/${id}`, {
-            headers: {
-                Authorization: `Bearer ${t}`,
-                'Content-Type': 'application/json'
-
-            }
-        })
-    } catch (err) {
-        return err
-    }
-}
+  try {
+    return axios.get(`/permissions/${id}`, {
+      headers: {
+        Authorization: `Bearer ${t}`,
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (err) {
+    return err;
+  }
+};
